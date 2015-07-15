817e29a305ce6557234c9e6ddc85ffced4177413
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2015-07-15 19:45:02 +0100

    Update install.sh
    *Should* have complete install.sh script now

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 echo "The installation wizard will now install everything required for this tutorial!"
 echo "This will take a long time... Why don't you go and get a cuppa?!"
+
 wget http://www.cmake.org/files/v3.3/cmake-3.3.0-rc2.tar.gz
 tar zxf cmake-3.3.0-rc2.tar.gz
 cd cmake-3.3.0-rc2/ 
@@ -14,4 +15,19 @@ wget http://archive.raspbian.org/raspbian/pool/main/libe/libevdev/libevdev-dev_1
 wget http://archive.raspbian.org/raspbian/pool/main/libe/libevdev/libevdev-tools_1.4.2%2bdfsg-1_armhf.deb
 wget http://archive.raspbian.org/raspbian/pool/main/libe/libevdev/libevdev2_1.4.2%2bdfsg-1_armhf.deb
 sudo dpkg -i libevdev*
+cd
 
+git clone https://github.com/the-raspberry-pi-guy/game_stream
+cd game_stream/Moonlight
+tar xf moonlight-embedded-2.0.2.tar.xz -C /home/pi/game_stream/
+cd ..
+sudo rm -r Moonlight
+cd moonlight-embedded-2.0.2/
+mkdir build
+cd build/
+sudo cmake ../
+sudo make
+sudo make install
+cd ~
+
+echo "*Tentative* And... We should be all done now!"
