69ebb5a761d54441c8b75858552d71e3c8592231
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2015-07-15 20:07:54 +0100

    Push update to install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ echo "The installation wizard will now install everything required for this tuto
 echo "This will take a long time... Why don't you go and get a cuppa?!"
 wget http://www.cmake.org/files/v3.3/cmake-3.3.0-rc2.tar.gz
 tar zxf cmake-3.3.0-rc2.tar.gz
-cd cmake-3.3.0-rc2/ 
+cd cmake-3.3.0-rc2 
 sudo ./bootstrap
 sudo make
 sudo make install
