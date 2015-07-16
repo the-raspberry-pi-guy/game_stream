e73c1f76538e8c87f0030a31fb0075eeccc40132
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2015-07-16 14:02:21 +0100

    Add an update clause earlier

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 echo "The installation wizard will now install everything required for this tutorial!"
 echo "This will take a long time... Why don't you go and get a cuppa?!"
 
+sudo apt-get update
 wget http://www.cmake.org/files/v3.3/cmake-3.3.0-rc2.tar.gz
 tar zxf cmake-3.3.0-rc2.tar.gz
 cd cmake-3.3.0-rc2/
@@ -9,7 +10,6 @@ sudo make
 sudo make install
 cd ..
 
-sudo apt-get update
 sudo apt-get -y install libopus-dev libexpat1-dev libssl-dev libasound2-dev libudev-dev libavahi-client-dev libcurl4-openssl-dev libjs-jquery
 wget http://archive.raspbian.org/raspbian/pool/main/libe/libevdev/libevdev-dev_1.4.2%2bdfsg-1_armhf.deb
 wget http://archive.raspbian.org/raspbian/pool/main/libe/libevdev/libevdev-tools_1.4.2%2bdfsg-1_armhf.deb
